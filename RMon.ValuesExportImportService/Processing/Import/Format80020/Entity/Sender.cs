0217f6c38bacfc77d0d6b38d732a129939d3fb0b
Кадысев Сергей Александрович <s.kadyisev@allmonitoring.ru>
2021-06-18 23:13:35 +0300

    ROM-2770: Добавлена структура формата 80020

---
+++ RMon.ValuesExportImportService/Processing/Import/Format80020/Entity/Sender.cs
@@ -0,0 +1,13 @@
+﻿using System.Xml.Serialization;
+
+namespace RMon.ValuesExportImportService.Processing.Import.Format80020.Entity
+{
+    public class Sender
+    {
+        [XmlElement("name")]
+        public string Name { get; set; }
+
+        [XmlElement("inn")]
+        public string Inn { get; set; }
+    }
+}
---
+++ RMon.ValuesExportImportService/Processing/Import/Format80020/Entity/Value.cs
@@ -0,0 +1,13 @@
+﻿using System.Xml.Serialization;
+
+namespace RMon.ValuesExportImportService.Processing.Import.Format80020.Entity
+{
+    public class Value
+    {
+        [XmlAttribute("status")]
+        public string Code { get; set; }
+
+        [XmlText]
+        public string Val { get; set; }
+    }
+}
